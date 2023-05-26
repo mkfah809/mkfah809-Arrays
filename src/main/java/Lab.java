533fea478e3b657819628d2c0571e7179352a80e
Mina K. Fahmy <minakaramm1@gmail.com>
2023-05-26 03:09:03 +0000

    return all the array Nth size

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -3,10 +3,13 @@
  * An array is a group of some predefined number of elements.
  * For instance, we could have an array of 10 ints or 10 Strings.
  *
- * Or, we could have an array of our own custom Objects, such as our own Bird class for a birdwatching
- * application. This would allow us to keep track of many birds and their own behavior.
+ * Or, we could have an array of our own custom Objects, such as our own Bird
+ * class for a birdwatching
+ * application. This would allow us to keep track of many birds and their own
+ * behavior.
  *
- * For the following problems, all of the syntax we need is in this documentation:
+ * For the following problems, all of the syntax we need is in this
+ * documentation:
  * https://www.learnjavaonline.org/en/Arrays
  */
 public class Lab {
@@ -14,39 +17,42 @@ public class Lab {
      * @param arr the array we're getting data from.
      * @return the length of arr.
      */
-    public int getLengthOfArray(int[] arr){
-        return 0;
+    public int getLengthOfArray(int[] arr) {
+        return arr.length;
     }
-
-
 
     /**
      * @param arr the array we're getting data from.
-     * @param n the index (number) of the element we're looking to return.
-     *          Indexes in arrays start counting from zero.
+     * @param n   the index (number) of the element we're looking to return.
+     *            Indexes in arrays start counting from zero.
      * @return the element at index n of arr.
      */
-    public int getNthElementOfArray(int[] arr, int n){
-        return 0;
+    public int getNthElementOfArray(int[] arr, int n) {
+        return arr[n];
     }
 
     /**
      *
      * @param arr the array we're getting data from.
-     * @param n the index (number) of the element we're looking to modify.
-     *          Indexes in arrays start counting from zero.
+     * @param n   the index (number) of the element we're looking to modify.
+     *            Indexes in arrays start counting from zero.
      * @param val the value that we will assign to a spot in the array.
-     * @return nothing. Because of pass-by-reference, any change to the array will be reflected across our java program.
+     * @return nothing. Because of pass-by-reference, any change to the array will
+     *         be reflected across our java program.
      */
-    public void setNthElementOfArray(int[] arr, int n, int val){
-
+    public void setNthElementOfArray(int[] arr, int n, int val) {
+        arr[n] = val;
     }
 
     /**
      * @param n the size of the array we wish to create
      * @return a new int array that can contain n elements.
      */
-    public int[] returnNewArraySizeN(int n){
-        return null;
+    public int[] returnNewArraySizeN(int n) {
+        int[] oldArray = {1, 2, 3,5,6,8};
+        int[] newArray = new int[n];
+        System.arraycopy(oldArray, 0, newArray, 0, n);
+        
+        return newArray;
     }
 }
